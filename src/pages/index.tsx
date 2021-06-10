a6764fa3af092222544803b77448b87f5ab59d8f
Marcelo Ritter <marcelo.ritter@dotgroup.com.br>
2021-06-10 15:20:11 -0300

    Feat: Setting up global scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
 
 
 export default function Home() {
@@ -9,8 +8,8 @@ export default function Home() {
       <Head>
           <title>Início | ig.news</title>
         </Head>
-      <h1 className={styles.title}>
-        Hello <span>World</span>
+      <h1>
+        Hello World
       </h1>
     </>
   )

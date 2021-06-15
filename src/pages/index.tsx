35c36d4ef4de3e039ccd59b0b5e54f8d2cfec8be
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-15 15:35:39 -0300

    Feat: Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 
@@ -38,7 +38,7 @@ export default function Home( {product}: HomeProps ) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1J2dVhA3Kz0zZqBudmTYhIUy')
 
   const product = {
@@ -52,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
-import {version} from '../../package.json';
+import { version } from '../../package.json';
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,

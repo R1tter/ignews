b3a36298b57d945a30c0a570bf096034f272d9ea
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-17 17:15:07 -0300

    fix: lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
-
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 

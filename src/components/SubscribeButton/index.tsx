0438927315376db89e1aa376eca9462a86bb7101
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-22 15:24:41 -0300

    Feat: persisting payment info

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,13 +10,23 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session]= useSession();
 
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if (!session) {
       signIn('github')
       return;
     }
     
-    
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message);
+    }
   }
   return (
     <button 

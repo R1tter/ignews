b6ec9552d715b7c71dc8e5ce0762f7d6c9f6e02b
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-22 13:54:23 -0300

    Feat: Authentication with FaunaDB complete.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,23 +13,37 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
       try { 
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
         return true;
 
-      }catch {
+      }catch (err){
+        console.log(err);
         return false;
       }
 

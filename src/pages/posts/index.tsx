4a69f6b191ce11ef061d3eef885df36bef0e0dc4
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-29 11:25:03 -0300

    Feat: CMS config and posts endpoint

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,55 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+
+import { getPrismicClient } from '../../services/prismic';
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>28 de junho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process</p>
+          </a>
+          <a href="#">
+            <time>28 de junho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process</p>
+          </a>
+          <a href="#">
+            <time>28 de junho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+  console.log(response)
+
+  return {
+    props: {}
+  }
+}

344010d4168e9d51dd7c363f97696699fd3ead83
Marcelo Ritter <marcelosori@hotmail.com>
2021-06-29 13:50:59 -0300

    Feat: Active link next react.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
+import { useRouter } from 'next/router';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
